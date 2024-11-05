b5a618cfe43908c602877e3147d753aa9bf0484b
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-11-05 17:26:55 +0900

    [BOJ] 큐 2

---
+++ JeongHyunBaek/06강 - 큐/[BOJ] 18258 큐 2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String a = st.nextToken();
+
+            if ("push".equals(a)) {
+                String b = st.nextToken();
+                queue.offer(Integer.parseInt(b));
+            } else if ("pop".equals(a)) {
+                if (!queue.isEmpty()) {
+                    sb.append(queue.poll()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            } else if ("size".equals(a)) {
+                sb.append(queue.size()).append("\n");
+            } else if ("empty".equals(a)) {
+                if (queue.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if ("front".equals(a)) {
+                if (!queue.isEmpty()) {
+                    sb.append(queue.peek()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            } else {
+                if (!queue.isEmpty()) {
+                    sb.append(queue.peekLast()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
